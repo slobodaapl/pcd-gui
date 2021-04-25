cfac06dbe40293773816b1aab30307c5e6cd6961
Tibor Sloboda <xslobodat2@stuba.sk>
2021-04-25 18:34:04 +0200

    Safety improvements JavaDoc additions for Initializer, Main, ImageDataObject, ImageDataObjectFactory, ImageDataStorage

--- src/main/java/pcd/Initializer.java
+++ src/main/java/pcd/Initializer.java
@@ -13,13 +13,44 @@ import pcd.gui.MainFrame;
 import pcd.utils.Constant;
 import pcd.utils.FileUtils;
 
+/**
+ * @author Tibor Sloboda
+ * 
+ * Prepares missing directories and parses the configuration files, adding
+ * missing colors where necessary for cilia types.
+ *<p>
+ * Finally it instantiates {@link pcd.data.ImageDataStorage}, which in turn
+ * runs the Python process and starts the server. The GUI only opens once
+ * Python is connected and loaded.
+ * 
+ */
 public class Initializer {
     private static final Logger LOGGER = LogManager.getLogger(Initializer.class);
+    
+    /**
+     * List of cilia types
+     */
     private final ArrayList<String> typeConfigList = new ArrayList<>();
+    /**
+     * List of type IDs
+     */
     private final ArrayList<Integer> typeIdentifierList = new ArrayList<>();
+    /**
+     * List of color strings or icon paths
+     */
     private final ArrayList<String> typeIconList = new ArrayList<>();
+    /**
+     * List of the 'types' of the types of cilia (healthy/unhealthy)
+     */
     private final ArrayList<String> typeTypeList = new ArrayList<>();
 
+    /**
+     * Parses lines from the loaded configuration file.
+     * It splits the fields of the configuration into respective lists
+     * and adds random colors to those types that are missing it.
+     * 
+     * @param list The list of lines from the configuration file
+     */
     private void splitConfig(ArrayList<String> list) {
         for (int i = 0; i < list.size(); i++) {
             String string = list.get(i);
@@ -69,6 +100,10 @@ public class Initializer {
         }
     }
     
+    /**
+     * Creates missing folders and reads the config file using {@link pcd.utils.FileUtils{
+     * before passing the contents into {@link pcd.Initializer#splitConfig(java.util.ArrayList)}
+     */
     void setup(){
         File f = new File("Logs");
         if(!f.exists())
@@ -87,6 +122,19 @@ public class Initializer {
         FileUtils.prepCache();
     }
 
+    /**
+     * Initializes the {@link pcd.data.ImageDataStorage} and runs the primary GUI.
+     * It decides which initializer to use for the GUI based on whether or not
+     * a project file was present in passed arguments to main.
+     * 
+     * Finally it adds a listener to the main GUI window to stop the process and exit the program successfully.
+     * A {@link System#exit(0)} is used to make the program exit once the process is terminated. This is the
+     * last line of code in the project, so it is safe from unhandled exceptions.
+     * 
+     * @see MainFrame
+     * 
+     * @param projectFile The path in {@link String} format to any passed project files
+     */
     void run(String projectFile) {
         
         setup();
